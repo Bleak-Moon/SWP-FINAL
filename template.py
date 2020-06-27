1689abfc5bc41ee361854dfee5465899e53cc893
Bleak-Moon <lhj09100511@gmail.com>
2020-06-28 02:59:18 +0900

    issue #1 ADD WSGI APPLICATION

---
+++ template.py
@@ -0,0 +1,13 @@
+html=b"""
+<html>
+	<body>
+		<form method="get" action="">
+			a=<input type="number" name="a"><br>
+			b=<input type="number" name="b"><br>
+			<input type="submit">
+		</form>
+		sum= %(sum)s <br><br>
+		nul= %(mul)s
+	</body>
+</html>
+"""
