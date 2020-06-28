38f9d8e88c66d98cabcb217c704ae24b54cf3de2
Bleak-Moon <lhj09100511@gmail.com>
2020-06-28 15:02:39 +0900

    issue #3 CHANGE THE BASIC LINES PRINTED IN THE FIRST SCREEN: READY TO GET THE NUMBER

--- calc/fig.py
+++ calc/fig.py
@@ -1,19 +1,22 @@
 from cgi import parse_qs
 import matplotlib as mpl
 mpl.use("Agg")
-import mathplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from template import html
 
 def application(environ, start_response):
 	d=parse_qs(environ["QUERY_STRING"])
 	a=d.get("a", [""])[0]
 	b=d.get("b", [""])[0]
-	sum=0
-	mul=0
+	sum="합이 출력될 예정입니다"
+	mul="곱이 출력될 예정입니다"
 	if "" not in [a, b]:
 		a, b=int(a), int(b)
 		sum=a+b
 		mul=a*b
+
+
+
 	response_body=html % {
 	"sum":sum,
 	"mul":mul,
